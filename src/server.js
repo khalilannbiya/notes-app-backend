dd7a9ec4ee6cc7f21799477fc5ebdac842400c26
khalilannbiya <syeichkhalil@gmail.com>
2023-02-21 17:33:57 +0700

    init project notes app backend

---
+++ src/server.js
@@ -0,0 +1,26 @@
+/**
+ * Berkas ini menampung kode untuk
+ * membuat, mengonfigurasi, dan menjalankan HTTP server menggunakan Hapi
+ */
+
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
