a7db42f5a598663de63256babccc15fa03a64d6b
khalilannbiya <syeichkhalil@gmail.com>
2023-02-24 18:24:56 +0700

    feat: Creating Custom Errors

---
+++ src/exceptions/InvariantError.js
@@ -0,0 +1,16 @@
+/* eslint-disable max-len */
+/**
+ * InvariantError (extends dari ClientError) : Custom error yang mengindikasikan eror karena kesalahan
+ * bisnis logic pada data yang dikirimkan oleh client.
+ * Kesalahan validasi data merupakan salah satu InvariantError.
+ */
+const ClientError = require('./ClientError');
+
+class InvariantError extends ClientError {
+  constructor(message) {
+    super(message);
+    this.name = 'InvariantError';
+  }
+}
+
+module.exports = InvariantError;
---
+++ src/exceptions/NotFoundError.js
@@ -0,0 +1,15 @@
+/**
+ * NotFoundError (extends dari ClientError) : Custom error yang mengindikasikan eror karena
+ * resource yang diminta client tidak ditemukan
+ */
+
+const ClientError = require('./ClientError');
+
+class NotFoundError extends ClientError {
+  constructor(message) {
+    super(message, 404);
+    this.name = 'NotFoundError';
+  }
+}
+
+module.exports = NotFoundError;
